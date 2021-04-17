03c8a038bf20e7154fad0b594dfcb166a09265c2
JDHomere <jahmel.homer@my.uwi.edu>
2021-04-17 05:31:35 +0000

    Model/Update 3

--- App/models/courses.py
+++ App/models/courses.py
@@ -6,10 +6,10 @@ import datetime
 class Courses(db.Model):
     courseID = db.Column(db.Integer, primary_key=True)
     jobID = db.Column(db.Integer, db.ForeignKey('jobs.jobID'), nullable=False)
-    courseName = db.Column(db.String(80), nullable=False)
-    courseDescription = db.Column(db.String(1000), nullable=False)
-    skills = db.Column(db.String(100), nullable=False)
-    jobs = db.relationship('jobs')
+    courseName = db.Column(db.String(80), nullable=True)
+    courseDescription = db.Column(db.String(1000), nullable=True)
+    skills = db.Column(db.String(100), nullable=True)
+    jobs = db.relationship('Jobs')
     def toDict(self):
         return {
             "courseID": self.courseID,

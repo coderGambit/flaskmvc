6132050638271efbc13cdb411d88d364fd23a28b
JDHomere <jahmel.homer@my.uwi.edu>
2021-04-18 17:03:09 +0000

    User/UserMixin

--- App/models/user.py
+++ App/models/user.py
@@ -1,10 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 db = SQLAlchemy()
 import datetime
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
--- App/views/user.py
+++ App/views/user.py
@@ -1,9 +1,6 @@
 from flask import Blueprint, redirect, render_template, request, jsonify, send_from_directory
-
 user_views = Blueprint('user_views', __name__, template_folder='../templates')
-
 from App.models import User
-
 from App.controllers import(get_users_json, get_users, create_user)
 from App.forms import LogIn
 
